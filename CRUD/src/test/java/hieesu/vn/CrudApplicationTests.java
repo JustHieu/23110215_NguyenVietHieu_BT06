754d6ecb23d8b1366b9a00c5d1570614453fab55
JustHieu <hieusanta456@gmail.com>
2025-09-17 23:24:54 +0700

    First

---
+++ CRUD/src/test/java/hieesu/vn/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package hieesu.vn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
